0e83b24750b149bdd77106d3816bed3bfcd93183
OnkoRonko <nik.trofimov.2000@mial.ru>
2021-05-30 22:03:02 +0300

    Update documentation

--- main.py
+++ main.py
@@ -80,18 +80,17 @@ def main(hparams):
     cross_entropy = tf.keras.losses.SparseCategoricalCrossentropy(
         from_logits=True, reduction='none')
 
-  def loss_function(y_true, y_pred):
-    y_true = tf.reshape(y_true, shape=(-1, hparams.max_length - 1))
-    loss = cross_entropy(y_true, y_pred)
-    mask = tf.cast(tf.not_equal(y_true, 0), dtype=tf.float32)
-    loss = tf.multiply(loss, mask)
-    return tf.reduce_mean(loss)
+    def loss_function(y_true, y_pred):
+      y_true = tf.reshape(y_true, shape=(-1, hparams.max_length - 1))
+      loss = cross_entropy(y_true, y_pred)
+      mask = tf.cast(tf.not_equal(y_true, 0), dtype=tf.float32)
+      loss = tf.multiply(loss, mask)
+      return tf.reduce_mean(loss)
 
-  def accuracy(y_true, y_pred):
-    y_true = tf.reshape(y_true, shape=(-1, hparams.max_length - 1))
-    return tf.keras.metrics.sparse_categorical_accuracy(y_true, y_pred)
+    def accuracy(y_true, y_pred):
+      y_true = tf.reshape(y_true, shape=(-1, hparams.max_length - 1))
+      return tf.keras.metrics.sparse_categorical_accuracy(y_true, y_pred)
 
-  if(hparams.ready==0):
     model.compile(optimizer, loss=loss_function, metrics=[accuracy])
     model.fit(dataset, epochs=hparams.epochs)
     model.save_weights('./save/')
@@ -105,20 +104,20 @@ if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   parser.add_argument(
       '--max_samples',
-      default=25000,
+      default=50000,
       type=int,
       help='maximum number of conversation pairs to use')
   parser.add_argument(
       '--max_length', default=40, type=int, help='maximum sentence length')
-  parser.add_argument('--batch_size', default=64, type=int)
+  parser.add_argument('--batch_size', default=256, type=int, help='data in epoch')
   parser.add_argument('--num_layers', default=2, type=int)
   parser.add_argument('--num_units', default=512, type=int)
   parser.add_argument('--d_model', default=256, type=int)
   parser.add_argument('--num_heads', default=8, type=int)
   parser.add_argument('--dropout', default=0.1, type=float)
   parser.add_argument('--activation', default='relu', type=str)
-  parser.add_argument('--epochs', default=20, type=int)
-  parser.add_argument('--ready', default=0, type=int)
+  parser.add_argument('--epochs', default=10, type=int)
+  parser.add_argument('--ready', default=1, type=int, help='0 model training, 1 run of the finished model')
 
   hparams = parser.parse_args()
   main(hparams)
